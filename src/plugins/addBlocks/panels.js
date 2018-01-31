43fd49b7c67d4e427dacf3720b1547c22d476132
Olivier Monnier <omonnier.dev@gmail.com>
2018-01-31 18:07:39 +0100

    stylize

--- src/plugins/addBlocks/panels.js
+++ src/plugins/addBlocks/panels.js
@@ -1,12 +1,18 @@
 export default function(editor, config = {}) {
   const { Panels } = editor
 
-  Panels.addButton('options', {
-    id: 'addBlocksButton',
-    className: 'fa fa-list-alt',
-    command: () => editor.runCommand('addBlocks'),
-    attributes: {
-      title: 'Add Blocks'
-    }
+  Panels.addPanel({
+    id: 'files',
+    visible: true,
+    buttons: [
+      {
+        id: 'addBlocksButton',
+        className: 'fa fa-list-alt',
+        command: () => editor.runCommand('addBlocks'),
+        attributes: {
+          title: 'Add Blocks'
+        }
+      }
+    ]
   })
 }
